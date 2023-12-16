0089cd77d0388e456d9ae1b5c4fe6f4ca074f6ca
Paul Wild <paul.wild@coop.co.uk>
2023-12-16 13:23:48 +0000

    Day 16

--- AdventOfCode/Days/Day12.cs
+++ AdventOfCode/Days/Day12.cs
@@ -2,41 +2,41 @@ namespace AdventOfCode.Days;
 
 public class ConditionRecord
 {
-    private readonly List<long> _springConditions;
+    private readonly List<int> _springConditions;
     private readonly char[] _conditionArray;
     
-    public ConditionRecord(string inputRow, long part = 1)
+    public ConditionRecord(string inputRow, int part = 1)
     {
         var repeat = part == 1 ? 1 : 5;
         var split = inputRow.Split(' ');
-        var numbers = split[1].Split(',').Select(x => Convert.ToInt64(x)).ToList();
+        var numbers = split[1].Split(',').Select(x => Convert.ToInt32(x)).ToList();
         _springConditions = Enumerable.Repeat(numbers, repeat).SelectMany(x => x).ToList();
         _conditionArray = string.Join('?', Enumerable.Repeat(split[0], repeat)).ToCharArray();
     }
 
-    private bool CanPlaceAt(long index, long hashLength)
+    private bool CanPlaceAt(int index, int hashLength)
     {
         if (index + hashLength == _conditionArray.Length)
         {
-            return _conditionArray.Skip((int)index).Take((int)hashLength).All(x => x != '.');
+            return _conditionArray.Skip(index).Take(hashLength).All(x => x != '.');
         }
         if (index + hashLength > _conditionArray.Length)
         {
             return false; 
         }
 
-        return _conditionArray.Skip((int)index).Take((int)hashLength).All(x => x != '.') 
+        return _conditionArray.Skip(index).Take(hashLength).All(x => x != '.') 
                && _conditionArray[index + hashLength] != '#';
     }
     
 
     public long Combinations()
     {
-        var potentials = new List<(long index, long times)> { (0,1) };
+        var potentials = new List<(int index, long times)> { (0,1) };
         
         foreach (var springCondition in _springConditions)
         {
-            var newPotentials = new List<(long index, long times)>();
+            var newPotentials = new List<(int index, long times)>();
             foreach (var (index, times) in potentials)
             {
                 newPotentials.AddRange(FindPotentialSpots(index, springCondition, times));
@@ -48,14 +48,14 @@ public class ConditionRecord
                 .ToList();
         }
 
-        return potentials.Where(x => _conditionArray.Skip((int)x.index).All(ch => ch != '#')).Select(x => x.times).Sum();
+        return potentials.Where(x => _conditionArray.Skip(x.index).All(ch => ch != '#')).Select(x => x.times).Sum();
 
     }
 
-    private IEnumerable<(long index, long time)> FindPotentialSpots(long index, long springCondition, long combos)
+    private IEnumerable<(int index, long time)> FindPotentialSpots(int index, int springCondition, long combos)
     
     {
-        var potentials = new List<long>();
+        var potentials = new List<int>();
         while (true)
         {
             if (index >= _conditionArray.Length || index > 0 && _conditionArray[index - 1] == '#')
---
+++ AdventOfCode/Days/Day16.cs
@@ -0,0 +1,143 @@
+using AdventOfCode.Common;
+using Delta = (int dx, int dy);
+using Location = (int x, int y);
+
+namespace AdventOfCode.Days;
+
+public class Day16 : ISolution
+{
+    public string PartOne(IEnumerable<string> input)
+    {
+        var inputList = input.ToList();
+        var maxX = inputList.First().Length;
+        var maxY = inputList.Count;
+        var map = ParseMap(inputList);
+
+        Stack<(Delta delta, Location location)> deltas = new([((1, 0), (-1, 0))]);
+
+        return Traverse(deltas, maxX, maxY, map).ToString();
+    }
+    
+    public string PartTwo(IEnumerable<string> input)
+    {
+        var inputList = input.ToList();
+        var maxX = inputList.First().Length;
+        var maxY = inputList.Count;
+        var map = ParseMap(inputList);
+        
+        Stack<(Delta delta, Location location)> deltas = new();
+        
+        for (var y = 0; y < maxY; y++)
+        {
+            //Left Spots 
+            deltas.Push(((1,0), (-1,y)));
+            //Right Spots
+            deltas.Push(((-1,0), (maxX,y)));
+        }
+
+        for (var x = 0; x < maxX; x++)
+        {
+            //Top Spots
+            deltas.Push(((0,1), (x,-1)));
+            //Bottom Spots
+            deltas.Push(((0,-1), (x,maxY)));
+        }
+
+        
+        return deltas.Select(start => Traverse(new Stack<(Delta delta, Location location)>([start]), maxX, maxY, map))
+            .Max()
+            .ToString();
+    }
+    
+    private List<Delta> GetDelta(char reflector, Delta currentDelta)
+    {
+        return reflector switch
+        {
+            '\\' when currentDelta.dx == 1 => [(0, 1)],
+            '\\' when currentDelta.dx == -1 => [(0, -1)],
+            '\\' when currentDelta.dy == -1 => [(-1, 0)],
+            '\\' when currentDelta.dy == 1 => [(1, 0)],
+            '/' when currentDelta.dx == 1 => [(0, -1)],
+            '/' when currentDelta.dx == -1 => [(0, 1)],
+            '/' when currentDelta.dy == -1 => [(1, 0)],
+            '/' when currentDelta.dy == 1 => [(-1, 0)],
+            '|' when currentDelta.dy != 0 => [currentDelta],
+            '|' => [(0, -1), (0, 1)],
+            '-' when currentDelta.dx != 0 => [currentDelta],
+            '-' => [(1, 0), (-1, 0)],
+            _ => throw new Exception("Nope")
+        };
+    }
+
+
+    private static Dictionary<Location, char> ParseMap(List<string> inputList)
+    {
+        Dictionary<Location, char> map = new();
+        foreach (var (row, y) in inputList.WithIndex())
+        foreach (var (square, x) in row.ToCharArray().WithIndex())
+        {
+            if (square != '.')
+            {
+                map.Add((x, y), square);
+            }
+        }
+
+        return map;
+    }
+
+    private int Traverse(Stack<(Delta delta, Location location)> deltas, int maxX, int maxY, Dictionary<Location, char> map)
+    {
+        HashSet<(Location location, Delta delta)> visited = new();
+        while (deltas.Count != 0)
+        {
+            var (delta, location) = deltas.Pop();
+            Location next = (location.x + delta.dx, location.y + delta.dy);
+
+            if (next.x >= maxX || next.y >= maxY || next.x < 0 || next.y < 0)
+            {
+                continue;
+            }
+
+            if (!visited.Add((next,delta)))
+            {
+                continue;
+            }
+
+            var nextDeltas = map.TryGetValue(next, out var value)
+                ? GetDelta(value, delta)
+                : [delta];
+
+            foreach (var d in nextDeltas)
+            {
+                deltas.Push((d, next));
+            }
+        }
+
+        // var visitedLocs = visited.Select(x => x.Item1).ToList();
+        // for (int y = 0; y < maxY; y++)
+        // {
+        //     var sb = new StringBuilder();
+        //     for (int x = 0; x < maxX; x++)
+        //     {
+        //
+        //         if (map.ContainsKey((x, y)))
+        //         {
+        //             sb.Append(map[(x, y)]);
+        //         }
+        //         else if (visitedLocs.Contains((x, y)))
+        //         {
+        //             sb.Append('#');
+        //         }
+        //         else
+        //         {
+        //             sb.Append('.');
+        //         }
+        //     }
+        //     Console.WriteLine(sb.ToString());
+        // }
+
+        return visited.Select(x => x.location).Distinct().Count();
+    }
+    
+    public int Day => 16;
+}
